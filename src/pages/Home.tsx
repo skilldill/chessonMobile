f3ad537aa2047ee82aa37567a80a8de747bcf669
Alexander <skilldill@yandex.ru>
2025-12-08 00:11:00 +0300

    Created chesson mobile

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Chesson Mobile</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <h1>Hello Chesson Mobile!</h1>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
