a582af025295186be30309013ae61efd03863b63
Alexander <skilldill@yandex.ru>
2025-12-16 22:50:58 +0300

    fix for storybook

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import { fileURLToPath } from 'node:url';
 
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+  // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig(async () => {
   const config: any = {
     plugins: [
