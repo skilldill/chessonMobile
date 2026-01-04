dc142a07f1e395645143732ec52851f5bcf21fb9
Alexander <skilldill@yandex.ru>
2026-01-04 20:12:59 +0300

    updates for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'chessonMobile',
+  appId: 'me.chesson.app',
+  appName: 'ChessOn Me',
   webDir: 'dist',
   server: {
     allowNavigation: ['chesson.me', '*.chesson.me'],
